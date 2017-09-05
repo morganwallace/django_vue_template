8901f38d2dde555d9c0458a2df2a2d20310c8bf7
Morgan Wallace <morgan@Morgans-MacBook.local>
2017-09-05 00:01:23 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Template for making Vue and Django web apps
--- fabfile.py
+++ fabfile.py
@@ -11,6 +11,7 @@ import os
 ROOT = os.path.join('/', 'Users', 'morgan', 'Developer', 'my-project')
 VENV_PATH = os.path.join(ROOT, 'venv', 'bin', 'activate')
 FRONTEND = os.path.join(ROOT, 'frontend')
+BACKEND = os.path.join(ROOT, 'backend')
 
 ###
 # Commands
@@ -21,3 +22,8 @@ def dev_frontend():
     with prefix('cd %s' % FRONTEND):
         local('npm run dev')
 
+def server():
+    """Shortcut for running server locally."""
+    with prefix('cd %s' % BACKEND):
+        with prefix('source %s' % VENV_PATH):
+            local('python manage.py runserver')
